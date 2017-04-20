657b131842800e63b4e33e2084d63224968672fe
rskbhv <rsk.bhv@gmail.com>
2017-04-20 16:29:42 +0530

    first commit

---
+++ src/main/java/weatherapp/WeatherReport.java
@@ -0,0 +1,7 @@
+package weatherapp;
+
+public class WeatherReport {
+	public double todayWeather() {
+		return 44.6;
+	}
+}
---
+++ src/test/java/weatherapp/WeatherReportTest.java
@@ -0,0 +1,12 @@
+package weatherapp;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class WeatherReportTest extends TestCase {
+	@Test
+	public void todayWeatherTest() {
+		assertEquals(44.6, 44.6, 0);
+	}
+}
